a7fc348578a61a4529e6258b5770e64c8c705ede
huttongrabiel <huttonthomas@icloud.com>
2023-01-09 14:59:25 -0800

    Switch from hugo to self written svelte website

---
+++ src/routes/+page.svelte
@@ -0,0 +1,40 @@
+<script>
+</script>
+
+<div class="menu-bar">
+    <ul >
+        <li class="hutton-grabiel-main"><a href="/">Hutton Grabiel</a></li>
+        <li><a href="/portfolio">Portfolio</a></li>
+        <li><a href="/about">About</a></li>
+    </ul>
+</div>
+
+<style>
+    :global(.hutton-grabiel-main) {
+        font-family: Iosevka;
+        font-weight: bolder;
+        font-size: large;
+        float: left;
+    }
+
+    :global(.menu-bar) {
+        border: 10px white;
+    }
+
+    :global(.menu-bar ul) {
+        list-style: none;
+        overflow: hidden;
+        font-family: Iosevka;
+        margin: 0;
+        padding: 0;
+        vertical-align: top;
+    }
+
+    :global(.menu-bar ul li a) {
+        float: right;
+        text-align: center;
+        color: black;
+        padding: 10px 10px;
+        text-decoration: none;
+    }
+</style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,7 @@
+<div class="menu-bar">
+    <ul >
+        <li class="hutton-grabiel-main"><a href="/">Hutton Grabiel</a></li>
+        <li><a href="/portfolio">Portfolio</a></li>
+        <li><a href="/about">About</a></li>
+    </ul>
+</div>
