6c9157bf95b429b7ed299fb128a01099a7170f04
huttongrabiel <huttonthomas@icloud.com>
2023-01-26 22:05:03 -0800

    Attempt to get SvelteKit page hosted on github pages

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,31 +1,36 @@
+<script>
+	import { base } from '$app/paths';
+</script>
+
 <div class="menu-bar">
-    <ul >
-        <li class="hutton-grabiel-main"><a href="/">Hutton Grabiel</a></li>
-        <li><a href="/portfolio">Portfolio</a></li>
-        <li><a href="/about">About</a></li>
-    </ul>
+	<ul>
+		<li class="hutton-grabiel-main"><a href="/">Hutton Grabiel</a></li>
+		<li><a href="/portfolio">Portfolio</a></li>
+		<li><a href="/about">About</a></li>
+	</ul>
 </div>
 
 <body>
-    <div class="about-body-text">
-        <p>
-        I was born and raised in Southern California but now live in Seattle, Washington.
-        I am currently pursuing a degree in computer science at the University of Washington - Seattle.
-        As you can tell by this website, my area of interest is backend development. In my free
-        time I like to code, exercise, skateboard, and surf (when I'm back in California).
-        </p>
-    </div>
+	<div class="about-body-text">
+		<p>
+			I was born and raised in Southern California but now live in Seattle, Washington. I am
+			currently pursuing a degree in computer science at the University of Washington - Seattle. As
+			you can tell by this website, my area of interest is backend development. In my free time I
+			like to code, exercise, skateboard, and surf (when I'm back in California).
+		</p>
+	</div>
 </body>
 
 <style>
-    body {
-        font-family: Iosevka;
-    }
+	body {
+		font-family: Iosevka;
+	}
 
-    .about-body-text {
-        text-align: center;
-        padding-left: 5%;
-        padding-right: 5%;
-        padding-top: 20%;
-    }
+	.about-body-text {
+		text-align: center;
+		padding-left: 5%;
+		padding-right: 5%;
+		padding-top: 20%;
+	}
 </style>
+
