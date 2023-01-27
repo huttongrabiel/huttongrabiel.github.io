c1dc0bcb6f90faef60b6924361136b141ab37326
huttongrabiel <60057590+huttongrabiel@users.noreply.github.com>
2023-01-27 21:52:10 +0000

    Deploying to gh-pages from @ huttongrabiel/huttongrabiel.github.io@171a3a7da06256c83af107b19a65dcca4824e882 🚀

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const error_template = ({ status, message }) => "<!DOCTYPE html>\n<html lang=\"e
 let read = null;
 
 set_paths({"base":"","assets":""});
-set_version("1674856233692");
+set_version("1674856326281");
 
 let default_protocol = 'https';
 
@@ -50,7 +50,7 @@ export class Server {
 			app_template,
 			app_template_contains_nonce: false,
 			error_template,
-			version: "1674856233692"
+			version: "1674856326281"
 		};
 	}
 
