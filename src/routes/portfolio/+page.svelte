d97e90c05cc3ad4edef115d2e3a539d7ae666db1
huttongrabiel <huttonthomas@icloud.com>
2023-07-26 21:07:47 -0700

    Overhaul: Basic website up

--- src/routes/portfolio/+page.svelte
+++
@@ -1,7 +0,0 @@
-<div class="menu-bar">
-    <ul >
-        <li class="hutton-grabiel-main"><a href="/">Hutton Grabiel</a></li>
-        <li><a href="/portfolio">Portfolio</a></li>
-        <li><a href="/about">About</a></li>
-    </ul>
-</div>
